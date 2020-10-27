6a57744978d8c2a6350b76175dfa11f4c05736e4
Erin Anderson <e.anderson1226@gmail.com>
2020-10-27 08:40:24 -0400

    commit tree f813dbd62adf14d13ca0aa1413a0f76767ff9c30

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)# put argument(s) here
+  #iterate over every element in an array
+  #calls on while
+  counter = 0 
+  while counter < array.length #the counter is less than the length of the array
+  #yield to the block for each individual element
+    yield(array[counter])
+    counter += 1
+  end
+  array
 end
